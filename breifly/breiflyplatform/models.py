04383774ae49b840857dad5c1ffa12fb3b33a499
Joan Mas Castella <33759606+Joanmascastella@users.noreply.github.com>
2025-03-18 10:38:29 +0800

    models done

--- breifly/breiflyplatform/models.py
+++ breifly/breiflyplatform/models.py
@@ -3,12 +3,12 @@ from django.utils.timezone import now
 
 # Supabase Users Table Model
 class User(models.Model):
-    id = models.UUIDField(primary_key=True)  # Supabase's user ID
+    id = models.UUIDField(primary_key=True)  
     email = models.EmailField(unique=True)
 
     class Meta:
         db_table = '"auth"."users"'
-        managed = False  # Prevent Django from managing this table
+        managed = False  
 
 # Roles Model
 class Role(models.Model):
@@ -18,7 +18,6 @@ class Role(models.Model):
     class Meta:
         db_table = 'roles'
         managed = False
-
 
 # User Roles Model
 class UserRole(models.Model):
@@ -35,3 +34,26 @@ class UserRole(models.Model):
         db_table = 'user_roles'
         managed = False
 
+# Item Model
+class Item(models.Model):
+    id = models.UUIDField(primary_key=True)  
+    serial_number = models.TextField()
+    provider = models.TextField(blank=True, null=True)
+    name = models.TextField()
+    category = models.TextField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'items'
+        managed = False  
+
+# Previous Offer Model
+class PreviousOffer(models.Model):
+    id = models.UUIDField(primary_key=True)  
+    items = models.JSONField()
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, db_column='user_id', related_name='previous_offers'
+    )
+
+    class Meta:
+        db_table = 'previous_offers'
+        managed = False  
